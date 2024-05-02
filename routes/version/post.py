343e9313b45d5f1a6f5f6387b2e1b040818d7a30
gaon12 <34927797+gaon12@users.noreply.github.com>
2024-05-02 22:28:23 +0900

    Changing version information in the .env would still use the old version until the server restarted Troubleshooting #29

--- routes/version/post.py
+++ routes/version/post.py
@@ -4,22 +4,16 @@ import datetime
 import os
 import sys
 
-# 환경 변수 관리 모듈 경로 추가 함수
-def add_env_manage_to_path():
-    # 현재 파일의 절대 경로를 구한 뒤, 상위 디렉토리로 이동하여 env_manage.py가 위치한 디렉토리의 경로를 구합니다.
-    current_file_path = os.path.dirname(os.path.abspath(__file__))
-    env_manage_directory = os.path.abspath(os.path.join(current_file_path, os.pardir, os.pardir))
-
-    # 이 경로를 sys.path의 시작 부분에 추가합니다.
-    sys.path.insert(0, env_manage_directory)
-
-add_env_manage_to_path()
-
-from env_manage import get_env_variable
-
-# 버전 정보를 불러오는 함수
+# 버전 정보를 version.txt 파일에서 불러오는 함수
 def get_service_version():
-    return get_env_variable('SERVICE_VERSION')
+    # version.txt 파일의 경로를 설정합니다.
+    current_file_path = os.path.dirname(os.path.abspath(__file__))
+    version_file_path = os.path.join(current_file_path, 'version.txt')
+
+    # 파일을 읽어 첫 번째 줄을 반환합니다.
+    with open(version_file_path, 'r') as file:
+        version = file.readline().strip()  # strip()을 사용하여 줄바꿈을 제거합니다.
+    return version
 
 # 현재 UTC 시간을 ISO 형식 문자열로 반환하는 함수
 def get_current_utc_time():
@@ -38,3 +32,4 @@ def post_response(request_data):
         }
     }
     return response_data, 200
+
